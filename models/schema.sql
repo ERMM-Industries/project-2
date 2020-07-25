0284f312881ca41b22cde0f46e8ff30abdc04543
Michael Bailey <michaeljaybailey@gmail.com>
2020-07-25 12:51:31 -0700

    added id

--- models/schema.sql
+++ models/schema.sql
@@ -13,6 +13,7 @@ CREATE TABLE IF NOT EXISTS `credentials` (
 INSERT INTO `credentials` (`username`, `password`, `email`,`first name`,`last name`,`birthday`) VALUES ('test', 'test', 'test@test.com', 'test','test','01/01/01');
 
 CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -25,5 +26,9 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `actors` varchar(500) NOT NULL,
   `date of release` varchar(50) NOT NULL,
   `plot summary` varchar(10000) NOT NULL,
-  'account status' varchar(100) NOT NULL,
+  `account status` varchar(100) NOT NULL,
+  `awards` varchar(20) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `accounts` ADD PRIMARY KEY (`id`);
+ALTER TABLE `accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
